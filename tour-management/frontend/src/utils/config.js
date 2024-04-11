04342d1660c641f7b23ad38e07fda0f995aa06cc
singamreddyanitha <127717845+singamreddyanitha@users.noreply.github.com>
2024-04-11 15:26:19 +0530

    Update config.js

--- tour-management/frontend/src/utils/config.js
+++ tour-management/frontend/src/utils/config.js
@@ -1,4 +1,4 @@
 
-// export const BASE_URL = "http://localhost:4000/api/v1"
+export const BASE_URL = "http://localhost:4000/api/v1"
 
-export const BASE_URL = "https://tour-and-travel-1c6r.onrender.com/api/v1"
+// export const BASE_URL = "https://tour-and-travel-1c6r.onrender.com/api/v1"
